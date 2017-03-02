26aa70d91955de58c295ac5b92bdc7caa8c49ac1
renwenbo <renwenbo@localhost.com>
2017-03-02 11:37:53 +0800

    update build.sh

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+go get github.com/nsf/gocode
+go get github.com/rogpeppe/godef
+go get golang.org/x/tools/cmd/goimports
+go get golang.org/x/tools/cmd/oracle
+go get golang.org/x/tools/cmd/gorename
+go get github.com/golang/lint/golint
+go get golang.org/x/tools/go/gcimporter15
+go get github.com/jstemmer/gotags 
