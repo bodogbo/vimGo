dbb8b9f995e82947b8a5906123f7f031fb2dfeca
ren wenbo <renwenbo@work.com>
2017-09-21 11:09:04 +0800

    update build.st:h

--- src/build.sh
+++ src/build.sh
@@ -8,3 +8,13 @@ go get golang.org/x/tools/cmd/gorename
 go get github.com/golang/lint/golint
 go get golang.org/x/tools/go/gcimporter15
 go get github.com/jstemmer/gotags 
+go get golang.org/x/tools/go/ast/astutil
+go get golang.org/x/tools/go/buildutil
+go get golang.org/x/tools/go/loader
+go get github.com/josharian/impl
+go get github.com/zmb3/gogetdoc
+go get github.com/fatih/motion
+go get github.com/klauspost/asmfmt/cmd/asmfmt
+go get github.com/kisielk/errcheck
+go get golang.org/x/tools/cmd/guru
+go get github.com/alecthomas/gometalinter
